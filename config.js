e8cad5f18c98bb603f78d367ea9f46c0674d8590
divya_mishra <divyamishra075@gmail.com>
2024-02-11 00:12:19 +0530

    'gatsby-temp'

---
+++ config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  pathPrefix: '',
+  siteUrl: 'https://rahul4code.github.io/portfolio',
+  siteTitle: 'Divya Mishra',
+  siteDescription: 'Logbook of a software developer',
+  author: 'Divya Mishra',
+  postsForArchivePage: 3,
+  defaultLanguage: 'en',
+  disqusScript: process.env.DISQUS_SCRIPT || 'https://rolwinreevan.disqus.com/embed.js',
+  pages: {
+    home: '/',
+    contact: 'contact',
+    resume: 'resume',
+    tag: 'tags',
+  },
+  social: {
+    linkedIn: 'https://linkedin.com/in/divya-mishra-10',
+    github: 'https://github.com/divya10mishra',
+    twitter: 'https://twitter.com/divyamishra_10',
+    medium: 'https://medium.com/@divyamishra10',
+    rss: '/rss.xml',
+  },
+  contactFormUrl: process.env.CONTACT_FORM_ENDPOINT || 'https://getform.io/f/573b6fdd-009c-48fe-98cb-df6ad2e240f3',
+  googleAnalyticTrackingId: process.env.GA_TRACKING_ID || '',
+  tags: {
+    javascript: {
+      name: 'javascript',
+      description: 'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.',
+      color: '#f0da50',
+    },
+    nodejs: {
+      name: 'Node.js',
+      description: 'Node.js is a tool for executing JavaScript in a variety of environments.',
+      color: '#90c53f',
+    },
+    typescript: {
+      name: 'typescript',
+      description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
+      color: '#257acc',
+    },
+    reactjs: {
+      name: 'reactjs',
+      description: 'React is an open source JavaScript library used for designing user interfaces.',
+      color: '#61dbfa',
+    },
+    gatsby: {
+      name: 'Gatsby.js',
+      description: 'A framework built over ReactJS to generate static page web application.  ',
+      color: '#6f309f',
+    },
+    html: {
+      name: 'HTML',
+      description: 'A markup language that powers the web. All websites use HTML for structuring the content.',
+      color: '#dd3431',
+    },
+    css: {
+      name: 'Swiggy Clone',
+      description: 'Cloned Swiggy to style the HTML element and to give a very fancy look for the web application.',
+      color: '#43ace0',
+    },
+    python: {
+      name: 'python',
+      description: 'A general purpose programming language that is widely used for developing various applications.',
+      color: '#f9c646',
+    },
+
+  },
+};
